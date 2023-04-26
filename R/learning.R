f9237d94fcaaa2037aaef6177155c327e3b847a8
Svenja <svenja@sund.ku.dk>
2023-04-26 13:20:51 +0200

    Added lines for chapter 7

--- R/learning.R
+++ R/learning.R
@@ -1,16 +1,114 @@
 
-# Basics of R -------------------------------------------------------------
-colnames(airquality)
-str(airquality)
-summary(airquality)
-1 + 1
-mean(2:6)
+# Data Management and Wrangling -----------------------------------------------------------
 
-
-# Loading packages --------------------------------------------------------
-
+# Load packages
 library(tidyverse)
+library(NHANES)
 
-#This will be used for testing out Git
+# Looking at data
+glimpse(NHANES)
 
-#Causing conflict
+# selecting columns
+select(NHANES, Age)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+# Create smaller NHANES dataset
+nhanes_small <- select(
+  NHANES, Age, Gender, BMI, Diabetes, PhysActive, BPSysAve,
+  BPDiaAve, Education
+)
+
+# Renaming columns
+nhanes_small <- rename_with(
+  nhanes_small,
+  snakecase::to_snake_case
+)
+
+# Renaming specific columns
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+# Trying out the pipe
+
+colnames(nhanes_small)
+
+nhanes_small %>%
+  colnames()
+
+nhanes_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+# Exercise 7.8
+
+# 1
+nhanes_small %>%
+  select(bp_sys_ave, education)
+
+# 2
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
+
+# 3 rewrite: select(nhanes_small, bmi, contains("age"))
+nhanes_small %>%
+  select(bmi, contains("age"))
+
+nhanes_small %>%
+  select(bmi, age)
+
+# 4 blood_pressure <- select(nhanes_small, starts_with("bp_"))
+# rename(blood_pressure, bp_systolic = bp_sys_ave)
+
+blood_pressure <- nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
+
+# Filtering
+nhanes_small %>%
+  filter(phys_active != "No")
+
+nhanes_small %>%
+  filter(bmi >= 25)
+
+# combining logical operators
+nhanes_small %>%
+  filter(bmi >= 25 & phys_active == "No")
+
+nhanes_small %>%
+  filter(bmi >= 25 | phys_active == "No")
+
+# Arrange data
+
+nhanes_small %>%
+  arrange(age)
+
+nhanes_small %>%
+  arrange(desc(age))
+
+nhanes_small %>%
+  arrange(education, age)
+
+
+# Transform data
+
+nhanes_small %>%
+  mutate(
+    age = age * 12, # having the age in months and not years
+    logged_bmi = log(bmi)
+  )
+
+nhanes_small %>%
+  mutate(old = if_else(age >= 30, "Yes", "No"))
+
+
